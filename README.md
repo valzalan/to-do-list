457f5d49dbbcee33e31a3a0432a0c25d79aba4a4
Zalan Valko <valzalan@gmail.com>
2018-08-03 22:30:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A typical To Do List, which can be found in basically every junior frontend dev's repos. (Maybe not with localStorage :))
+Thought I might as well do my own iteration.
---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+let localStorage = window.localStorage;
+let taskList = [];
+
+(function init() {
+
+  if( localStorage.taskList === undefined ) {
+
+    localStorage.setItem( "taskList", "" );
+
+  } else {
+
+    taskList = JSON.parse( localStorage.getItem( "taskList" ) );
+
+    for( let task of taskList ) {
+      appendToList( task );
+    }
+  }
+})();
+
+function appendToList( task ) {
+
+  const list = document.getElementById( "task-list" ),
+        newListItem = document.createElement( "li" );
+
+  newListItem.innerHTML = task;
+  newListItem.id = `task-${list.children.length + 1}`
+  newListItem.addEventListener("click", function(){ removeFromList(event); });
+  list.appendChild( newListItem );
+}
+
+function getTask(event) {
+
+  if(event.target.id == "button" || event.keyCode == 13){
+
+    const inputField = document.getElementById( "task-input" ),
+          task = inputField.value;
+
+    if( task != "" && task != undefined ) {
+      appendToList( task );
+      addToLocalStorage( task );
+    }
+    inputField.value = "";
+  }
+}
+
+function addToLocalStorage( task ) {
+
+  taskList.push( task );
+  localStorage.setItem( "taskList", JSON.stringify( taskList ) );
+}
+
+function removeFromLocalStorage( task ) {
+
+  taskIndex = Number(task.substring(5));
+  taskList.splice(taskIndex - 1, 1);
+  localStorage.setItem( "taskList", JSON.stringify( taskList ) );
+}
+
+function removeFromList(event) {
+
+  targetItem = document.getElementById(event.target.id);
+  removeFromLocalStorage(event.target.id);
+  targetItem.parentNode.removeChild(targetItem);
+}
